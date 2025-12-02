0a8a3572d2fc73d7a217dc65661277e72b8df08d
Eduaapp <eduardoaponteh@outlook.com>
2025-12-02 09:14:00 -0500

    Initialize Flask app with a home route

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hola Eduardo, esta es tu primera app DevOps."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
